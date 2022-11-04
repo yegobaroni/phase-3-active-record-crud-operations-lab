4ab178876a0182935ae6ff61a403af3debd5c01c
yego <yegokipkemoi254@gmail.com>
2022-11-05 00:39:14 +0300

    Done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,53 @@
 class Movie < ActiveRecord::Base
 
+
+    def self.create_with_title(title)
+     Movie.create(title:title)
+  end
+
+  def self.first_movie
+    all = Movie.all
+    all.first
+  end
+
+  def self.last_movie
+    all = Movie.all
+    all.last
+  end
+
+  def self.movie_count
+    all = Movie.all
+    all.count
+  end
+
+  def self.find_movie_with_id(id)
+    movie = Movie.find_by(id: id)
+    movie
+  end
+
+  def self.find_movie_with_attributes(attributes)
+    movie = Movie.find_by(attributes)
+    movie
+  end
+
+  def self.find_movies_after_2002
+    Movie.where("release_date>2002")
+  end
+
+def update_with_attributes(attributes)
+    self.update(attributes)
+  end
+def self.update_all_titles(title)
+    Movie.update_all(title:title)
+  end
+
+  def self.delete_by_id(id)
+    movie = Movie.find_by(id: id)
+    movie.delete
+  end
+
+  def self.delete_all_movies
+    Movie.destroy_all
+  end
+
 end
